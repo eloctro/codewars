887f05f7bf33c1dc1d7825296bb40c217e3abe1b
Dim <eloctro@tut.by>
2022-06-15 21:30:09 +0300

    solution

--- animals-and-inheritance/index.js
+++ animals-and-inheritance/index.js
@@ -1,39 +1,41 @@
-class Shark extends Animal {
+class Animal {
   constructor(name, age, legs, species, status) {
-    super(name, age, status);
-    this.legs = 0;
-    this.species = "shark";
+    this.name = name;
+    this.age = age;
+    this.legs = legs;
+    this.species = species;
     this.status = status;
   }
-
   introduce() {
-    super.introduce();
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+  }
+}
+
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 0, "shark", status);
   }
 }
 
 class Cat extends Animal {
   constructor(name, age, status) {
-    super(name, age, status);
-    this.species = 4;
+    super(name, age, 4, "cat", status);
   }
-
   introduce() {
-    return `Hello, my name is ${this.name} and I am ${this.age} years old.  Meow meow!`;
+    return `${super.introduce()}  Meow meow!`;
   }
 }
 
 class Dog extends Animal {
   constructor(name, age, status, master) {
-    super(name, age, status);
-    this.species = 4;
+    super(name, age, 4, "dog", status);
     this.master = master;
   }
-
-  introduce() {
-    super.introduce();
-  }
-
   greetMaster() {
     return `Hello ${this.master}`;
   }
 }
+
+let billy = new Shark("Billy", 3, "Alive and well");
+
+console.log(billy.introduce());
