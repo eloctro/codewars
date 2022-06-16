2d98a45324d3f24fe74e4ba0e12370d661f031f7
eloctro <eloctro@tut.by>
2022-06-16 14:14:32 +0300

    solution

---
+++ class-like-objects/index.js
@@ -0,0 +1,16 @@
+class Animal {
+    constructor(name, type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    toString() {
+        return `${this.name} is a ${this.type}`
+    }
+}
+
+let dog = new Animal('Max', 'dog');
+dog.toString(); // should return 'Max is a dog'
+dog.type; // should == 'dog'
+dog.name; // should == 'Max'
+dog.name = 'Lassie'; // should set name to 'Lassie'
